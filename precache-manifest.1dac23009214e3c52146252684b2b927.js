464a0f8715a3adb1e12abb77ad2fe3967e251a1b
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2021-07-29 03:00:57 +0530

    Updates

---
+++ precache-manifest.1dac23009214e3c52146252684b2b927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4854715d10b502153d6d8ff1dd1553",
+    "url": "/loremipsum-react/index.html"
+  },
+  {
+    "revision": "e5aa4ead7eb1842e83ca",
+    "url": "/loremipsum-react/static/css/main.fb86bce3.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/loremipsum-react/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/loremipsum-react/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5aa4ead7eb1842e83ca",
+    "url": "/loremipsum-react/static/js/main.5ca91827.chunk.js"
+  },
+  {
+    "revision": "32fb1ce2d67873c95d36",
+    "url": "/loremipsum-react/static/js/runtime-main.e5a52e82.js"
+  }
+]);
